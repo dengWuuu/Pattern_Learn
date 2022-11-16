e5859a7ca6bf754a1004df73b86fab31e3f8f836
Jackson_Wu <dengwu.wu@foxmail.com>
2022-11-16 15:43:08 +0800

    prototype

---
+++ src/main/java/org/wu/prototype/App.java
@@ -0,0 +1,16 @@
+package org.wu.prototype;
+
+public class App {
+   public static void main(String[] args) {
+      ShapeCache.loadCache();
+ 
+      Shape clonedShape = ShapeCache.getShape("1");
+      System.out.println("Shape : " + clonedShape.getType());        
+ 
+      Shape clonedShape2 = ShapeCache.getShape("2");
+      System.out.println("Shape : " + clonedShape2.getType());        
+ 
+      Shape clonedShape3 = ShapeCache.getShape("3");
+      System.out.println("Shape : " + clonedShape3.getType());        
+   }
+}
---
+++ src/main/java/org/wu/prototype/Square.java
@@ -0,0 +1,13 @@
+package org.wu.prototype;
+
+public class Square extends Shape {
+ 
+   public Square(){
+     type = "Square";
+   }
+ 
+   @Override
+   public void draw() {
+      System.out.println("Inside Square::draw() method.");
+   }
+}
